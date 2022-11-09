c40928cca8ebe75fdbf4c91a43cf259261d67f5f
Ramiro404 <ramirocruznavarro@hotmail.com>
2022-11-08 20:46:16 -0600

    Deploy again

--- src/pipes/parse-int/parse-int.pipe.ts
+++ src/pipes/parse-int/parse-int.pipe.ts
@@ -9,7 +9,7 @@ import {
 export class ParseIntPipe implements PipeTransform {
   transform(value: any, metadata: ArgumentMetadata) {
     const parsedInt = parseInt(value, 10);
-    if (isNaN(parsedInt)){
+    if (isNaN(parsedInt)) {
       throw new BadRequestException(`${value} is not a number`);
     }
     return parsedInt;
